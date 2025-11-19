4844903c628d8696ab6ad2302a546bb6f9d6fb6b
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-11-19 20:40:08 +0530

    examples of Streamlit

--- 14-StreamLit/app.py
+++ 14-StreamLit/app.py
@@ -1,6 +1,25 @@
 import streamlit as st
-import pandas as pt
+import pandas as pd
 import numpy as np
 
 # Title of the application
 st.title('Hello Streamlit')
+
+# Display Simple Text
+st.write("This is a simple Text")
+
+df = pd.DataFrame({
+    'first column': [1,2,3,4],
+    'second column': [10,20,30,40] 
+})
+
+# Display the dataframe
+st.write('Here is the dataframe')
+st.write(df)
+
+
+# Create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20,3), columns=['a','b','c']
+)
+st.line_chart(chart_data)
